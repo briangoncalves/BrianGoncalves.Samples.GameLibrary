942b819b903b9891b0e1b8f476bf3e54d74d5963
Brian Goncalves <briancollog@hotmail.com>
2017-09-15 09:26:24 -0300

    started the client side
    started the client side using vue.js
    using auth0 for facebook authentication
    using bootstrap for css layout
    node to install packages
    webpack to do deploy / execution
    Run
    "npm run dev" to start a dev web server and open the brower with the data.

---
+++ GameLibrary/Client/GameLibraryClient/src/App.vue
@@ -0,0 +1,83 @@
+﻿<template>
+  <div>
+    <nav class="navbar navbar-default fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">Game Library App</a>
+          <!--<router-link :to="'/'"
+                       class="btn btn-primary btn-margin">
+            Home
+          </router-link>
+
+          <button class="btn btn-primary btn-margin"
+                  v-if="!authenticated"
+                  @click="login()">
+            Log In
+          </button>
+          <button class="btn btn-primary btn-margin"
+                  v-if="authenticated"
+                  :to="'/games'">
+            Games
+          </button>
+
+          <button class="btn btn-primary btn-margin"
+                  v-if="authenticated"
+                  @click="logout()">
+            Log Out
+          </button>-->
+          </div>
+          <div class="collapse navbar-collapse">
+            <ul class="navbar-nav nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a v-if="!authenticated" @click="login()">Login</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" v-if="authenticated" href="/games">Games</a>
+              </li>
+              <li class="nav-item">
+                <span class="nav-link" v-if="authenticated" @click="logout()" href="#">Logout</span>
+              </li>
+            </ul>            
+          </div>          
+        </div>
+    </nav>
+
+    <div class="container">
+      <router-view :auth="auth"
+                   :authenticated="authenticated">
+      </router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+  import 'jquery'
+  import 'auth0-js'
+  import 'bootstrap'
+  import 'bootstrap/dist/css/bootstrap.min.css'
+  import AuthService from './auth/AuthService'
+  const auth = new AuthService()
+  const { login, logout, authenticated, authNotifier } = auth
+  export default {
+    name: 'app',
+    data () {
+      authNotifier.on('authChange', authState => {
+        this.authenticated = authState.authenticated
+      })
+      return {
+        auth,
+        authenticated
+      }
+    },
+    methods: {
+      login,
+      logout
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ GameLibrary/Client/GameLibraryClient/src/components/Hello.vue
@@ -0,0 +1,16 @@
+﻿<template>
+  <div class="hello">
+    <h1>Welcome to the Game Library</h1>
+    <p>Here you can add all the game titles you want</p>
+    <p v-if="!authenticated">Log in at the top to start using</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {}
+  }
+}
+</script>
